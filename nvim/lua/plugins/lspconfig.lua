5617770ab55e3582d1b0c16a7c09d2687d8d0863
Brad Evans <brad.evans0925@gmail.com>
2025-02-11 15:44:31 +0000

    actaully enable LSP's...

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,23 +1,25 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   ---@class PluginLspOpts
-  --   opts = {
-  --     ---@type lspconfig.options
-  --     servers = {
-  --       -- pyright will be automatically installed with mason and loaded with lspconfig
-  --       -- pyright = {},
-  --       -- bashls = {},
-  --       -- gopls = {},
-  --       -- bicep = {},
-  --       -- yamlls = {},
-  --       -- marksman = {},
-  --       -- powershell_es = {},
-  --       -- azure_pipelines_ls = {},
-  --       -- terraformls = {},
-  --       -- helm_ls = {},
-  --       -- csharp_ls = {},
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    --   ---@class PluginLspOpts
+    opts = {
+      --     ---@type lspconfig.options
+      servers = {
+        --      pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        bashls = {},
+        gopls = {},
+        bicep = {},
+        yamlls = {},
+        marksman = {},
+        powershell_es = {},
+        azure_pipelines_ls = {},
+        terraformls = {
+          filetypes = { "terraform", "hcl" },
+        },
+        helm_ls = {},
+        csharp_ls = {},
+      },
+    },
+  },
 }
