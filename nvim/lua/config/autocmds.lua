4b6081c35d972bb8216bb5d6f957fbd67eace7e6
Brad Evans <brad.evans0925@gmail.com>
2025-03-11 19:01:28 +0000

    -

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,7 +6,7 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "markdown" },
   callback = function()
-    require("cmp").setup({ enabled = false })
+    -- require("cmp").setup({ enabled = false })
   end,
 })
 
@@ -15,9 +15,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "gitcommit", "markdown", "pandoc" },
   callback = function()
     require("cmp").setup({ enabled = false })
-    vim.opt_local.textwidth = 80
-    vim.opt_local.formatoptions:append("a")
-    vim.opt_local.colorcolumn = "80"
+    -- vim.opt_local.textwidth = 80
+    -- vim.opt_local.formatoptions:append("a")
+    -- vim.opt_local.colorcolumn = "80"
   end,
 })
 
